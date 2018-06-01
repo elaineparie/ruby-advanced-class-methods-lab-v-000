9105586d830e8fb98604ce4187442b9c131807f9
Elaine Anderson <eparie@bu.edu>
2018-06-01 17:29:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,9 +54,9 @@ end
 #"Taylor Swift - Blank Space.mp3"
 def self.new_from_filename(file_name)
   normalized_name = []
-  normalized_name = file_name.split('-')
-  name = normalized_name[1].gsub('.mp3', '').strip
-  artist_name = normalized_name[0].strip
+  normalized_name = file_name.split(' - ')
+  name = normalized_name[1].gsub('.mp3', '')
+  artist_name = normalized_name[0]
   song = self.new
   song.name = name
   song.artist_name = artist_name
